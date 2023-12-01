cbcaa9a1ee96978eb4243182e2e4f7bff6b55be8
shenli0920 <102562467+shenli0920@users.noreply.github.com>
2023-11-30 19:26:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ Created on Tue Oct 24 10:52:03 2023
 @author: lshen
 """
 import numpy as np
-from persistence import calculate
-from functions import read_file,plot_betti_lap,plot_betti_lap_channels,process
+from functions import read_file,process
 
 ##chose your parameters
 file = 'X1.xy' # the 'xy' file, 'xyz' file or 'mol2' file you want to study,including 'X1.xy','X2.xyz','C20.xyz','C60.xyz','CB7.mol2'.
